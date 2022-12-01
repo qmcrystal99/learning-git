96f507876b90c6519122ff7e05d65b612ece2414
xuzhenhui <zhenhui.xu@thoughtworks.com>
2022-12-01 18:56:20 +0800

    temp commit

--- feature1.tsx
+++ feature1.tsx
@@ -1,3 +1,13 @@
 function feature1() {
+    const number = 1
+    switch (number) {
+        case 1:
+            console.log(`number = 1`);
+            break;
+        default:
+            console.log(`number is unknow`);
+            
+            break;
+    }
     return 1;
 }
