8cf6ef51ee10785473eda56768774f10f3d3ba81
min.qing <min.qing@thoughtworks.com>
2022-12-01 19:18:29 +0800

    [min ] add function3

--- feature1.tsx
+++ feature1.tsx
@@ -6,8 +6,10 @@ function feature1() {
             break;
         case 2: 
             console.log(`number = 2`);
+            break
         case 3: 
             console.log(`number = 3`);
+            break;
         default:
             console.log(`number is unknow`);
             break;
@@ -17,6 +19,14 @@ function feature1() {
 
 function feature2(a: number) {
     if ( a > 1 && a < 5) {
+        return 2;
+    } else {
+        return 0;
+    }
+}
+
+function feature3(b: number) {
+    if ( b > 1 && b < 5) {
         return 2;
     } else {
         return 0;
